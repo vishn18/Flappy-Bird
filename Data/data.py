6f281ecf3dfa59f7e5d5987a289e3c76a8a42aa6
VRMCode <vishnurmenon18@gmail.com>
2021-06-25 14:32:34 +0530

    Updated data.py

--- Data/data.py
+++ Data/data.py
@@ -1,30 +1,22 @@
-try:
-    from colorama import Fore
-except ModuleNotFoundError:
-    print("Couldn't import colorama!")
 import csv
-
-
-def get_data():
-    data = {}
-    with open("data.csv", "r") as file:
-        reader = csv.reader(file)
-        for line in reader:
-            data["highscore"] = line[0]
-            data["defaultbird"] = line[1]
-    return data
+import os
 
 
 def reset():
-    with open("data.csv", "w", newline="") as file:
+    with open(os.path.join("Data", "data.csv"), "w", newline="") as file:
         file.write("0,1")
 
 
-with open("data.csv", "r") as file:
-    if file.read() == "":
-        print("Data file is empty!")
-        print("Resetting data...")
-        reset()
-        print("\rResetting data... -done!")
+def get():
+    data = {}
+    with open(os.path.join("Data", "data.csv"), "r") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            data["highscore"] = line[0]
+            data["bird"] = line[1]
+    return data
 
-print(get_data())
+
+def edit(hs, bird):
+    with open(os.path.join("Data", "data.csv"), "w", newline="") as file:
+        file.write(f"{hs},{bird}")
