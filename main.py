6855a8ceaf1afea4f46b2c102f010880b68f1eca
VRMCode <vishnurmenon18@gmail.com>
2021-06-25 09:36:09 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ WIN = pygame.display.set_mode(
     (WIDTH, HEIGHT)
 )
 pygame.display.set_caption("Flappy Bird")
-ICON = pygame.image.load(
-    os.path.join("Assets", "icon.png")
-)
+ICON = pygame.image.load(os.path.join("Assets", "Images", "icon.png"))
 pygame.display.set_icon(ICON)
 
 # Game
 FPS = 60
+INVINCIBLE = 0
 up = False
 score = 0
 
@@ -34,44 +33,60 @@ BROWN = (153, 79, 23)
 BIRD_IMG = pygame.transform.rotate(
     pygame.transform.flip(
         pygame.image.load(
-            os.path.join("Assets", "bird.png")
+            os.path.join("Assets", "Images", "bird.png")
         ),
         False,
         True,
     ),
     180,
 )
+SEAGULL_IMG = pygame.transform.rotate(
+    pygame.transform.flip(
+        pygame.image.load(
+            os.path.join("Assets", "Images", "seagull.png")
+        ),
+        False,
+        True,
+    ),
+    180,
+)
+AIRCRAFT_IMG = pygame.image.load(os.path.join("Assets", "Images", "aircraft.png"))
+COPTER_IMG_1 = pygame.image.load(os.path.join("Assets", "Images", "helicopter.png"))
+COPTER_IMG_2 = pygame.image.load(os.path.join("Assets", "Images", "helicopter2.png"))
+cur_bird_img = BIRD_IMG
 BIRD_FALL_VEL = 3
 BIRD_FALL_VEL_CHANGE = 0.05
 bird_cur_fall_vel = BIRD_FALL_VEL
-BIRD_CLIMB_VEL = 70
-BIRD_BOUNCE_VEL = BIRD_CLIMB_VEL - 20
+BIRD_CLIMB_VEL = 10
+BIRD_BOUNCE_VEL = 50
 
 # Ground
 GROUND = pygame.Rect(0, 600, 600, 50)
 
 # Obstacles
-OBS = pygame.Rect(WIDTH - 50, 0, 50, HEIGHT)
 OBS_VEL = 5
-OBS_GAP = BIRD_IMG.get_height() + 100
+obs_gap = cur_bird_img.get_height() + 150
 OBS_SPACING = 100
-OBS_PATH = pygame.Rect(
-    OBS.x,
-    random.randint(
-        0,
-        (HEIGHT - GROUND.height - OBS_GAP)
-    ),
-    OBS.width,
-    OBS_GAP,
+OBS_TOP = pygame.Rect(
+    WIDTH - 50,
+    0,
+    50,
+    random.randint(0, (HEIGHT - GROUND.height - obs_gap)),
+)
+OBS_BOTTOM = pygame.Rect(
+    OBS_TOP.x,
+    OBS_TOP.height + obs_gap,
+    OBS_TOP.width,
+    (HEIGHT - GROUND.height) - OBS_TOP.height,
 )
 
 # Fonts
 MAJOR_MONO_DISPLAY = pygame.font.Font(
-    os.path.join("Assets", "MajorMonoDisplay-Regular.ttf"),
+    os.path.join("Assets", "Fonts", "MajorMonoDisplay-Regular.ttf"),
     60
 )
 STAATLICHES = pygame.font.Font(
-    os.path.join("Assets", "Staatliches-Regular.ttf"),
+    os.path.join("Assets", "Fonts", "Staatliches-Regular.ttf"),
     40
 )
 
@@ -91,11 +106,15 @@ def fall(bird):
     global up, bird_cur_fall_vel
     bird_cur_fall_vel += BIRD_FALL_VEL_CHANGE
     if bird.colliderect(GROUND):
-        jump(bird)
+        up = True
+        bird_cur_fall_vel = BIRD_FALL_VEL
+        bird.y -= BIRD_BOUNCE_VEL
+        up = False
     elif not up:
         bird.y += bird_cur_fall_vel
-    if bird.colliderect(OBS) and not bird.colliderect(OBS_PATH):
-        end()
+    if not INVINCIBLE:
+        if bird.colliderect(OBS_TOP) or bird.colliderect(OBS_BOTTOM):
+            end()
 
 
 def jump(bird):
@@ -103,18 +122,26 @@ def jump(bird):
     bird_cur_fall_vel = BIRD_FALL_VEL
     up = True
     bird.y -= BIRD_CLIMB_VEL
-    time.sleep(0.05)
     up = False
 
 
+def move_bird(bird):
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_SPACE] and bird.y - BIRD_CLIMB_VEL > 0:
+        jump(bird)
+
+
 def obs_move():
-    global score
-    if OBS.x + OBS.width < 0:
-        OBS.x = WIDTH - OBS.width
-        OBS_PATH.y = random.randint(0, (HEIGHT - GROUND.height - OBS_GAP))
+    global obs_gap, score
+    obs_gap = cur_bird_img.get_height() + 150
+    if OBS_TOP.x + OBS_TOP.width < 0:
+        OBS_TOP.x = WIDTH - OBS_TOP.width
+        OBS_TOP.height = random.randint(0, (HEIGHT - GROUND.height - obs_gap))
         score += 1
-    OBS.x -= OBS_VEL
-    OBS_PATH.x = OBS.x
+    OBS_TOP.x -= OBS_VEL
+    OBS_BOTTOM.x = OBS_TOP.x
+    OBS_BOTTOM.y = OBS_TOP.height + obs_gap
+    OBS_BOTTOM.height = (HEIGHT - GROUND.height) - OBS_TOP.height
 
 
 def draw(bird):
@@ -129,18 +156,19 @@ def draw(bird):
     pygame.draw.rect(
         WIN,
         GREEN,
-        OBS
+        OBS_TOP,
     )
     pygame.draw.rect(
         WIN,
-        WHITE,
-        OBS_PATH
+        GREEN,
+        OBS_BOTTOM,
     )
     obs_move()
 
     # Bird
+    move_bird(bird)
     WIN.blit(
-        BIRD_IMG,
+        cur_bird_img,
         (bird.x, bird.y)
     )
     fall(bird)
@@ -160,10 +188,10 @@ def main():
     Main game loop
     :return: None
     """
-    global up, bird_cur_fall_vel
+    global cur_bird_img, up, bird_cur_fall_vel
     run = True
     clock = pygame.time.Clock()
-    bird = pygame.Rect(50, 100, BIRD_IMG.get_width(), BIRD_IMG.get_height())
+    bird = pygame.Rect(50, 100, cur_bird_img.get_width(), cur_bird_img.get_height())
     obstacles = []
     while run:
         clock.tick(FPS)
@@ -171,8 +199,16 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    jump(bird)
+                if event.key == pygame.K_1:
+                    cur_bird_img = BIRD_IMG
+                elif event.key == pygame.K_2:
+                    cur_bird_img = SEAGULL_IMG
+                elif event.key == pygame.K_3:
+                    cur_bird_img = AIRCRAFT_IMG
+                elif event.key == pygame.K_4:
+                    cur_bird_img = COPTER_IMG_1
+                elif event.key == pygame.K_5:
+                    cur_bird_img = COPTER_IMG_2
         draw(bird)
     pygame.quit()
 
